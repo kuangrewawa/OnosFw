1d4c809c4df2455fbb8853b4f525c1fb76750ecd
CNlucius <lukai1@huawei.com>
2015-08-06 15:31:00 +0800

    clean project

--- providers/ovsdb/host/src/main/java/org/onosproject/ovsdb/provider/host/OvsdbHostProvider.java
+++ providers/ovsdb/host/src/main/java/org/onosproject/ovsdb/provider/host/OvsdbHostProvider.java
@@ -111,13 +111,13 @@ public class OvsdbHostProvider extends AbstractProvider implements HostProvider 
             switch (event.type()) {
             case PORT_ADDED:
                 HostId hostId = HostId.hostId(subject.hwAddress(), null);
-                DeviceId deviceId = DeviceId.deviceId(uri(subject.dpid()));
+                DeviceId deviceId = DeviceId.deviceId(uri(subject.dpid().value()));
                 PortNumber portNumber = PortNumber.portNumber(subject
-                        .portNumber(), subject.portName());
+                        .portNumber().value(), subject.portName().value());
                 HostLocation loaction = new HostLocation(deviceId, portNumber,
                                                          0L);
                 DefaultAnnotations annotations = DefaultAnnotations.builder()
-                        .set("ifaceid", subject.ifaceid()).build();
+                        .set("ifaceid", subject.ifaceid().value()).build();
                 HostDescription hostDescription = new DefaultHostDescription(
                                                                              subject.hwAddress(),
                                                                              VlanId.vlanId(),
@@ -137,9 +137,9 @@ public class OvsdbHostProvider extends AbstractProvider implements HostProvider 
 
     }
 
-    public URI uri(long value) {
+    public URI uri(String value) {
         try {
-            return new URI("of", toHex(value), null);
+            return new URI("of", toHex(Long.valueOf(value)), null);
         } catch (URISyntaxException e) {
             return null;
         }
